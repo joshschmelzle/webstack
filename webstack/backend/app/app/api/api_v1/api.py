02a454fd18fe7582665530b3141dcbc408c650aa
Josh Schmelzle <13954434+joshschmelzle@users.noreply.github.com>
2020-12-14 22:38:46 -0500

    prototype of diagnostics API endpoint

--- webstack/backend/app/app/api/api_v1/api.py
+++ webstack/backend/app/app/api/api_v1/api.py
@@ -1,12 +1,16 @@
 from fastapi import APIRouter
 
-from app.api.api_v1.endpoints import fpms, network, profiler, speedtest, utils
+from app.api.api_v1.endpoints import (diagnostics, fpms, network, profiler,
+                                      speedtest, utils)
 
 api_router = APIRouter()
 
 api_router.include_router(speedtest.router, prefix="/speedtest", tags=["speedtest"])
 api_router.include_router(fpms.router, prefix="/fpms", tags=["front panel menu system"])
 api_router.include_router(utils.router, prefix="/utils", tags=["utilities"])
+api_router.include_router(
+    diagnostics.router, prefix="/diagnostics", tags=["diagnostics"]
+)
 api_router.include_router(profiler.router, prefix="/profiler", tags=["profiler"])
 api_router.include_router(
     network.router, prefix="/network", tags=["network information"]
