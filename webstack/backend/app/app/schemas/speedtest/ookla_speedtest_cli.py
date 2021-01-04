1a728fd3acdacde2c12f877548c7d4222e1cc207
Josh Schmelzle <13954434+joshschmelzle@users.noreply.github.com>
2021-01-04 01:21:45 -0500

    move back to pydantic basemodel

--- webstack/backend/app/app/schemas/speedtest/ookla_speedtest_cli.py
+++ webstack/backend/app/app/schemas/speedtest/ookla_speedtest_cli.py
@@ -1,31 +1,26 @@
-import dataclasses
 from typing import Optional
 
 from pydantic import BaseModel, Field
 
 
-@dataclasses.dataclass
-class Ping:
+class Ping(BaseModel):
     jitter: float = Field(example=3.971)
     latency: float = Field(example=11.151)
 
 
-@dataclasses.dataclass
-class Download:
+class Download(BaseModel):
     bandwidth: int = Field(example=19780836)
     dl_bytes: int = Field(example=226938080)
     elapsed: int = Field(example=11707)
 
 
-@dataclasses.dataclass
-class Upload:
+class Upload(BaseModel):
     bandwidth: int = Field(example=1364478)
     ul_bytes: int = Field(example=5226800)
     elapsed: int = Field(example=3813)
 
 
-@dataclasses.dataclass
-class Interface:
+class Interface(BaseModel):
     internal_ip: str = Field(example="192.168.1.21")
     name: str = Field(example="eth0")
     macAddr: str = Field(example="02:01:78:00:00:00")
@@ -33,8 +28,7 @@ class Interface:
     externalIp: str = Field(example="46.156.79.21")
 
 
-@dataclasses.dataclass
-class Server:
+class Server(BaseModel):
     server_id: int = Field(example=20398)
     name: str = Field(example="Rando LLC")
     location: str = Field(example="NYC")
@@ -44,8 +38,7 @@ class Server:
     ip: str = Field(example="243.238.215.215")
 
 
-@dataclasses.dataclass
-class Result:
+class Result(BaseModel):
     result_id: str = Field(example="949308a2-e34e-2fe0-81f7-8d12cc02daab")
     url: str = Field(
         example="https://www.speedtest.net/result/c/949308a2-e34e-2fe0-81f7-8d12cc02daab"
