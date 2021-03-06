4d57dbc2f29e2b8e0e64ae47863cc3b481e4576d
Josh Schmelzle <13954434+joshschmelzle@users.noreply.github.com>
2021-03-06 12:08:03 -0500

    pass in --accept-license arg to bypass eula

--- webstack/backend/app/app/services/ookla_speedtest_cli_service.py
+++ webstack/backend/app/app/services/ookla_speedtest_cli_service.py
@@ -2,5 +2,5 @@ from .helpers import run_cli_async
 
 
 async def get_speedtest_results() -> dict:
-    result = await run_cli_async("speedtest -f json")
+    result = await run_cli_async("speedtest -f json --accept-license")
     return result
