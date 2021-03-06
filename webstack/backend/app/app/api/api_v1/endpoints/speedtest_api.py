56dbdec36f1fc96b1f78ab0e54cd277d37929416
Josh Schmelzle <13954434+joshschmelzle@users.noreply.github.com>
2021-03-06 12:09:28 -0500

    add runcommanderror handler

--- webstack/backend/app/app/api/api_v1/endpoints/speedtest_api.py
+++ webstack/backend/app/app/api/api_v1/endpoints/speedtest_api.py
@@ -4,8 +4,9 @@ import logging
 from fastapi import APIRouter, Response
 
 from app.models.validation_error import ValidationError
+from app.models.runcommand_error import RunCommandError
 from app.schemas import speedtest
-from app.services import ookla_speedtest_cli_service
+from app.services import helpers, ookla_speedtest_cli_service
 
 router = APIRouter()
 
@@ -24,5 +25,9 @@ async def run_ookla_speedtest():
         return json.loads(resp)
     except ValidationError as ve:
         return Response(content=ve.error_msg, status_code=ve.status_code)
+    except RunCommandError as rce:
+        return Response(
+            content=rce.error_msg, status_code=rce.status_code
+        )
     except Exception as ex:
         return Response(content=str(ex), status_code=500)
